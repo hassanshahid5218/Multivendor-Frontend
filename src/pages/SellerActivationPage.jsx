963834a9934cef9824b7987d151ac2d1b0b74666
Muhammad hassan <hassanshahid5218@gmail.com>
2026-06-21 12:33:49 +0500

    update server.js

--- src/pages/SellerActivationPage.jsx
+++ src/pages/SellerActivationPage.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React, { useState } from 'react'
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom'
-import { frontend_url } from '../server';
+
 import { server } from '../server';
 const SellerActivationPage = () => {
     const {activation_token}=useParams();
--- src/server.js
+++ src/server.js
@@ -1,3 +1,2 @@
-export const server="https://multivendor-backend-five.vercel.app/api/v2";
-export const backend_url="https://multivendor-backend-five.vercel.app/";
-export const frontend_url="http://localhost:3000/api/v2"
+export const server="https://multivendor-backend-gamma.vercel.app/api/v2";
+export const backend_url="https://multivendor-backend-gamma.vercel.app/";
