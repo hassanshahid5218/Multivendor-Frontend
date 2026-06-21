1917956ddfc28c51a53c3a047f2536b3feb2a33c
Muhammad hassan <hassanshahid5218@gmail.com>
2026-06-21 10:42:56 +0500

    adding backend link

--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
-export const server="http://localhost:5000/api/v2";
-export const backend_url="http://localhost:5000/";
+export const server="https://multivendor-backend-five.vercel.app/api/v2";
+export const backend_url="https://multivendor-backend-five.vercel.app/";
 export const frontend_url="http://localhost:3000/api/v2"
